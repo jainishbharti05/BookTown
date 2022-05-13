e7de4cc5185bdbb9e2dbd28f0dbe808bf7449705
Jainish-Bharti <jainish.bharti@datagrokr.com>
2022-05-13 14:22:42 +0530

    Added a Procfile.

--- app.js
+++ app.js
@@ -55,6 +55,12 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+} else {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // Listening to the port
---
+++ client/build/precache-manifest.e5626c9fbd3b43896701e79a78a21585.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31e10fcdd43ffc71ae1d49fe028a9f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5310c115a07637e2dd6",
+    "url": "/static/css/main.dd22124b.chunk.css"
+  },
+  {
+    "revision": "1dd372430ae0bad8cc56",
+    "url": "/static/js/2.2abda3fe.chunk.js"
+  },
+  {
+    "revision": "e5310c115a07637e2dd6",
+    "url": "/static/js/main.e644a69e.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
